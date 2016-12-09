a808fb5bbfc17a64d5a840deb34d7aee13dc2d09
Roger Hart 2 <rogerhart2@rogers-mbp.home>
2016-12-09 12:53:31 +0000

    Just added a comment

--- iOS/GameViewController.swift
+++ iOS/GameViewController.swift
@@ -14,6 +14,7 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // an added remark
         
         let scene = GameScene.newGameScene()
 
